afef5e5a31cc0212b4b89317410c1160b1158191
Christof Marti <chrmarti@microsoft.com>
2020-07-28 17:56:21 +0200

    Apply if unchanged

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as https from 'https';
 
 import { ConfigurationTarget, ExtensionContext, commands, workspace, WorkspaceConfiguration } from 'vscode';
 
-const gistId = 'eafc6c48f8de6a6f4703ad4f4697cb53';
-
 interface Gist {
 	id: string;
 	files: Record<string, GistFile>;
@@ -19,27 +17,47 @@ interface GistHistory {
 	version: string;
 }
 
+const gistId = 'eafc6c48f8de6a6f4703ad4f4697cb53';
+
+const appliedVersionKey = 'persona.appliedVersion';
+
 export function activate(context: ExtensionContext) {
 
 	console.log('Congratulations, your extension "Persona" is now active!');
 
 	context.subscriptions.push(
 		commands.registerCommand('persona.applySettings', async () => {
-
-			const buf = await httpsGet(`https://api.github.com/gists/${gistId}`);
-			const gist = JSON.parse(buf.toString()) as Gist;
-			const settings = JSON.parse(gist.files['settings.json'].content);
-
 			const config = workspace.getConfiguration();
-			await mergeSettings(config, settings);
+			const previousVersion = config.get<string>(appliedVersionKey);
+			
+			const { settings, version } = await getGistSettings(gistId);
+			if (version === previousVersion) {
+				console.log('Persona: Nothing to do.');
+				return;
+			}
+			
+			const previousSettings = previousVersion && (await getGistSettings(gistId, previousVersion)).settings || undefined;
+
+			await mergeSettings(config, settings, previousSettings);
+			await config.update(appliedVersionKey, version, ConfigurationTarget.Global);
 		})
 	);
 }
 
-async function mergeSettings(config: WorkspaceConfiguration, settings: any) {
+async function getGistSettings(id: string, sha?: string) {
+	const shaSegment = sha ? `/${sha}` : '';
+	const buf = await httpsGet(`https://api.github.com/gists/${id}${shaSegment}`);
+	const gist = JSON.parse(buf.toString()) as Gist;
+	const settings = JSON.parse(gist.files['settings.json'].content);
+	const version = gist.history[0].version;
+	return { settings, version };
+}
+
+async function mergeSettings(config: WorkspaceConfiguration, settings: any = {}, previousSettings: any = {}) {
 	for (const key in settings) {
-		const theme = config.inspect(key);
-		if (!theme?.globalValue) {
+		const currentValue = config.inspect(key)?.globalValue;
+		const previousValue = previousSettings[key];
+		if (currentValue === previousValue) {
 			const value = settings[key];
 			await config.update(key, value, ConfigurationTarget.Global);
 		}
