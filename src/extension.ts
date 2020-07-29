cf090e3d45ea9217fcb93c126fe13b7ad0f51ed9
Christof Marti <chrmarti@microsoft.com>
2020-07-29 09:12:36 +0200

    Remove Settings command

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as https from 'https';
 
-import { ConfigurationTarget, ExtensionContext, commands, workspace, WorkspaceConfiguration } from 'vscode';
+import { ConfigurationTarget, ExtensionContext, commands, workspace, WorkspaceConfiguration, window, env, Uri } from 'vscode';
 
 interface Gist {
 	id: string;
+	html_url: string;
 	files: Record<string, GistFile>;
 	history: GistHistory[];
 }
@@ -21,15 +27,46 @@ const gistId = 'eafc6c48f8de6a6f4703ad4f4697cb53';
 
 const appliedVersionKey = 'persona.appliedVersion';
 
-export async function activate() {
-	await applySettings();
+export async function activate(context: ExtensionContext) {
+	await applyCurrentSettings();
+
+	context.subscriptions.push(
+		commands.registerCommand('persona.removeSettings', removeSettings),
+	);
 }
 
-async function applySettings() {
+async function applyCurrentSettings() {
+	const { settings, version } = await getGistSettings(gistId);
+	await applySettings(settings, version);
+}
+
+async function removeSettings() {
+	const config = workspace.getConfiguration();
+	const previousVersion = config.get<string>(appliedVersionKey);
+	if (!previousVersion) {
+		const message = 'There is no applied version in the settings and without that the settings to remove cannot be determined. You might have already removed them.';
+		await window.showInformationMessage(message, { modal: true });
+		return;
+	}
+
+	const removeSettings = 'Remove Settings';
+	const showAppliedSettings = 'Show Applied Settings';
+	const message = 'If one of your personal settings happends to be equal to the currently applied setting, that setting will be removed too.';
+	const result = await window.showWarningMessage(message, { modal: true }, removeSettings, showAppliedSettings);
+	if (result === removeSettings) {
+		await applySettings({}, undefined);
+		const message = 'Done. You might want to uninstall the Persona extension to avoid having the settings reapplied when VS Code starts.';
+		await window.showInformationMessage(message, { modal: true });
+	} else if (result === showAppliedSettings) {
+		const gist = await getGistSettings(previousVersion);
+		await env.openExternal(Uri.parse(gist.html_url));
+	}
+}
+
+async function applySettings(settings: any, version: string | undefined) {
 	const config = workspace.getConfiguration();
 	const previousVersion = config.get<string>(appliedVersionKey);
 	
-	const { settings, version } = await getGistSettings(gistId);
 	if (version === previousVersion) {
 		console.log('Persona: Nothing to do.');
 		return;
@@ -46,7 +83,8 @@ async function getGistSettings(idOrVersion: string) {
 	const gist = JSON.parse(buf.toString()) as Gist;
 	const settings = JSON.parse(gist.files['settings.json'].content);
 	const version = `${gist.id}/${gist.history[0].version}`;
-	return { settings, version };
+	const { html_url } = gist;
+	return { settings, version, html_url };
 }
 
 async function mergeSettings(config: WorkspaceConfiguration, settings: any = {}, previousSettings: any = {}) {
@@ -56,6 +94,15 @@ async function mergeSettings(config: WorkspaceConfiguration, settings: any = {},
 		if (currentValue === previousValue) {
 			const value = settings[key];
 			await config.update(key, value, ConfigurationTarget.Global);
+		}
+	}
+	for (const key in previousSettings) {
+		if (!(key in settings)) {
+			const currentValue = config.inspect(key)?.globalValue;
+			const previousValue = previousSettings[key];
+			if (currentValue === previousValue) {
+				await config.update(key, undefined, ConfigurationTarget.Global);
+			}
 		}
 	}
 }
